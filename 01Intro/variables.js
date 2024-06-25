badcff86323552c6d2e577205123caef29b5a741
Shubham <chaudhary2001shubham@gmail.com>
2024-06-25 16:28:27 +0530

    + variables

---
+++ 01Intro/variables.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello world";
+greetings.charAt(0);
+console.log(greetings);
+var userId = 23234;
+console.log(userId.toFixed(2));
+var isLoggedIn = false;
---
+++ 01Intro/variables.ts
@@ -0,0 +1,18 @@
+let greetings: string = "Hello world";
+
+greetings.charAt(0);
+console.log(greetings);
+
+let userId = 23234;
+console.log(userId.toFixed(2));
+let isLoggedIn: boolean = false;
+
+let hero: string;
+
+function getHero() {
+  return "thor";
+}
+
+hero = getHero();
+
+export {};
