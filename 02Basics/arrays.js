e99b89d19305ee459d470342387c6d8f0ec2a604
Shubham <chaudhary2001shubham@gmail.com>
2024-06-26 19:54:14 +0530

    Arrays

---
+++ 02Basics/arrays.js
@@ -0,0 +1,18 @@
+var superHeoros = [];
+// const heroPower: number[] = [];
+var heroPower = [];
+superHeoros.push("Spiderman");
+var allUsers = [
+    { name: "cheeta", isActive: false },
+    { name: "shubham", isActive: true },
+];
+allUsers.push({ name: "user", isActive: true });
+console.log(allUsers);
+var MLModels = [
+    [23, 42, 55],
+    [11, 22, 33],
+];
+var Lambda = [
+    [23, 42, 55],
+    [11, 22, 33],
+];
---
+++ 02Basics/arrays.ts
@@ -0,0 +1,30 @@
+const superHeoros: string[] = [];
+// const heroPower: number[] = [];
+
+const heroPower: Array<number> = [];
+
+superHeoros.push("Spiderman");
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [
+  { name: "cheeta", isActive: false },
+  { name: "shubham", isActive: true },
+];
+
+allUsers.push({ name: "user", isActive: true });
+console.log(allUsers);
+
+const MLModels: number[][] = [
+  [23, 42, 55],
+  [11, 22, 33],
+];
+
+const Lambda: Array<number[]> = [
+  [23, 42, 55],
+  [11, 22, 33],
+];
+
