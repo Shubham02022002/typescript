bdc9718bb1c41e291b499835adf5e5fcf6ced95e
Shubham <chaudhary2001shubham@gmail.com>
2024-06-27 16:15:29 +0530

    Tuples

---
+++ 02Basics/tuples.js
@@ -0,0 +1,6 @@
+// const user: (string | number)[] = [1, "shubham"];
+var user = ["shubham", 1, true];
+var rgb = [255, 255, 255];
+var user1 = [1, "shubham"];
+user1[1] = "user";
+// user1.push(true);
---
+++ 02Basics/tuples.ts
@@ -0,0 +1,12 @@
+// const user: (string | number)[] = [1, "shubham"];
+
+const user: [string, number, boolean] = ["shubham", 1, true];
+
+let rgb: [number, number, number] = [255, 255, 255];
+
+type User = [number, string];
+
+const user1: User = [1, "shubham"];
+
+user1[1] = "user";
+// user1.push(true);
