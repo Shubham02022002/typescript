1eb96f81b3dcf6b09d2c6933b747fee9aa5681f1
Shubham <chaudhary2001shubham@gmail.com>
2024-06-30 22:29:19 +0530

    Classes

--- 03PureTS/src/index.js
+++ 03PureTS/src/index.js
@@ -1,1 +1,9 @@
-console.log("typescript OP");
+var User = /** @class */ (function () {
+    function User(id, email, name) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+    }
+    return User;
+}());
+var user1 = new User(123, "user@gmail.com", "new user");
--- 03PureTS/src/index.ts
+++ 03PureTS/src/index.ts
@@ -1,1 +1,12 @@
-console.log("typescript OP");
+class User {
+  id: number;
+  email: string;
+  name: string;
+  constructor(id: number, email: string, name: string) {
+    this.id = id;
+    this.email = email;
+    this.name = name;
+  }
+}
+
+const user1 = new User(123, "user@gmail.com", "new user");
