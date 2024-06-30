3fb11ad258e6710c64cbc900e25f7485da0d716f
Shubham <chaudhary2001shubham@gmail.com>
2024-06-30 21:26:31 +0530

    More on interfaces

--- 02Basics/interfaces.js
+++ 02Basics/interfaces.js
@@ -1,7 +1,18 @@
+var Admin1 = {
+    dbID: 123,
+    role: "Admin",
+    email: "xyz@gmail.com",
+    userId: 1234,
+    gitHubToken: "user/github.com",
+    googleID: "ksfdh34hr1hfdsa;1ihr3/1234/",
+    startTrial: function () { return "Good to go"; },
+    getCoupon: function (name) { return 50; },
+};
 var user1 = {
     dbID: 123,
     email: "xyz@gmail.com",
     userId: 1234,
+    gitHubToken: "user/github.com",
     googleID: "ksfdh34hr1hfdsa;1ihr3/1234/",
     startTrial: function () { return "Good to go"; },
     getCoupon: function (name) { return 50; },
--- 02Basics/interfaces.ts
+++ 02Basics/interfaces.ts
@@ -8,10 +8,31 @@ interface User {
   getCoupon(couponName: string): number;
 }
 
+// Also know as reopening of an interface !
+interface User {
+  gitHubToken: string;
+}
+
+interface Admin extends User {
+  role: "Admin" | "T.A" | "Learner";
+}
+
+const Admin1: Admin = {
+  dbID: 123,
+  role: "Admin",
+  email: "xyz@gmail.com",
+  userId: 1234,
+  gitHubToken: "user/github.com",
+  googleID: "ksfdh34hr1hfdsa;1ihr3/1234/",
+  startTrial: () => "Good to go",
+  getCoupon: (name: "FIRST50") => 50,
+};
+
 const user1: User = {
   dbID: 123,
   email: "xyz@gmail.com",
   userId: 1234,
+  gitHubToken: "user/github.com",
   googleID: "ksfdh34hr1hfdsa;1ihr3/1234/",
   startTrial: () => "Good to go",
   getCoupon: (name: "FIRST50") => 50,
